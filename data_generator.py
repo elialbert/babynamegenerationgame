46950209feb0e0cb3335f1d0c96018193dbf915a
Eli Albert <elialbert@gmail.com>
2024-03-10 20:44:18 -0500

    messing with algorithm

--- data_generator.py
+++ data_generator.py
@@ -18,8 +18,6 @@ def new_lmh_count():
 
 class NameGenerator:
     def __init__(self):
-        self.all_names = []
-        # self.all_syllables = []
         self.syl_count = defaultdict(int)
         self.syl_position_count = {0: defaultdict(int), 1: defaultdict(int), 2: defaultdict(int), 3: defaultdict(int), 4: defaultdict(int)}
         self.syl_position_usage_counts = {0: new_lmh_count(), 1: new_lmh_count(), 2: new_lmh_count(), 3: new_lmh_count(), 4: new_lmh_count()}
@@ -28,21 +26,21 @@ class NameGenerator:
     def parse(self):
         with open('names_split_top_50.txt', newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=' ')
-            for row in reader:
-                self.all_names.append(row[0])
+            for popularity_index, row in enumerate(reader):
                 syllables = [x.lower() for x in row[1].split('-')]
-                # self.all_syllables.extend(syllables)
+                scale = 0
+                if popularity_index < 50:
+                    scale = 3
+                elif popularity_index < 100:
+                    scale = 2
+                elif popularity_index < 150:
+                    scale = 1
                 for i, syl in enumerate(syllables):
-                    self.syl_count[syl] += 1
-                    self.syl_position_count[i][syl] += 1
-                    # if i > 0:
-                    #     self.subsequent_list[syllables[i-1]].append(syl)
-        # self.all_syllables = list(set(self.all_syllables))
-        # print(self.subsequent_list)
+                    self.syl_count[syl] += 1 + scale
+                    self.syl_position_count[i][syl] += 1 + scale
+        # print(self.syl_count)
 
     def find_freq_count(self, syl):
-        # look up in syl_count_high or null if not found
-        # print(syl, self.syl_count_middle)
         ch = self.syl_count_high.get(syl)
         cm = self.syl_count_middle.get(syl)
         cl = self.syl_count_low.get(syl)
@@ -60,10 +58,6 @@ class NameGenerator:
         self.syl_count_high = dict(sorted(self.syl_count.items(), key=lambda item: item[1], reverse=True)[:first_third])
         self.syl_count_middle = dict(sorted(self.syl_count.items(), key=lambda item: item[1], reverse=True)[first_third:first_third*2])
         self.syl_count_low = dict(sorted(self.syl_count.items(), key=lambda item: item[1], reverse=True)[first_third*2:])
-
-        # print('a', self.syl_count_high)
-        # print('b', self.syl_count_middle)
-        # print('c', self.syl_count_low)
 
         for position, entries in self.syl_position_count.items():
             # ie position is 0 and combo is lh, meaning it's a first syllable and it has
@@ -88,18 +82,14 @@ class NameGenerator:
             return random.choice(choices[second_third:])
 
     def compute_params(self, intelligence, strength, speed, honesty, compassion):
-        # accepts 5 scores as input
-        total_frequency_high_count = strength
+        total_frequency_high_count = strength * 2 + math.ceil(speed / 2)
         total_frequency_middle_count = speed + compassion
-        total_frequency_low_count = intelligence + honesty
-        top_frequency_high_count = strength + honesty
-        top_frequency_middle_count = compassion
-        top_frequency_low_count = intelligence + speed
+        total_frequency_low_count = (intelligence * 2) + math.ceil(honesty / 2)
         syllable_2_count = strength + speed
         syllable_3_count = strength + speed + compassion
         syllable_4_count = math.floor(intelligence/1.1 + honesty)
         syllable_5_count = math.floor(intelligence + compassion/1.8)
-        deviation_high_count = intelligence
+        deviation_high_count = math.ceil(intelligence * 1.9)
         deviation_middle_count = honesty + compassion
         deviation_low_count = strength + speed
         total_frequency_total_count = total_frequency_high_count + total_frequency_middle_count + total_frequency_low_count
@@ -110,15 +100,6 @@ class NameGenerator:
             total_frequency = 'm'
         else:
             total_frequency = 'h'
-
-        total_top_frequency_total_count = top_frequency_high_count + top_frequency_middle_count + top_frequency_low_count
-        random_total_top_frequency = random.randint(0, total_top_frequency_total_count)
-        if random_total_top_frequency < top_frequency_low_count:
-            total_top_frequency = 'l'
-        elif random_total_top_frequency < top_frequency_low_count + top_frequency_middle_count:
-            total_top_frequency = 'm'
-        else:
-            total_top_frequency = 'h'
 
         syllable_total_count = syllable_2_count + syllable_3_count + syllable_4_count + syllable_5_count
         random_syllable = random.randint(1, syllable_total_count)
@@ -142,25 +123,29 @@ class NameGenerator:
         else:
             deviation = 'h'
 
-        return {'total_frequency': total_frequency, 'total_top_frequency': total_top_frequency, 'num_syllables': num_syllables, 'deviation': deviation}
+        return {'total_frequency': total_frequency, 'num_syllables': num_syllables, 'deviation': deviation}
 
     def generate_name(self, intelligence, strength, speed, honesty, compassion):
         params = self.compute_params(intelligence, strength, speed, honesty, compassion)
-        print(params)
+        # print(params)
         name = []
 
         for i in range(params['num_syllables']):
+            if i == 3:
+                i = random.choice([2, 3])
             syl_choice = self.syl_position_usage_counts[i][params['total_frequency']]
+
             sorted_syl_choice = dict(sorted(syl_choice.items(), key=lambda item: item[1], reverse=True))
+            # print('poss', i, params['deviation'], sorted_syl_choice)
             try:
                 syl = self.choose_per_deviation(list(sorted_syl_choice.keys()), params['deviation'])
+                # print('got', syl)
+                name.append(syl)
             except IndexError:
-                syl_choice = self.syl_position_usage_counts[i-1][params['total_frequency']]
-                sorted_syl_choice = dict(sorted(syl_choice.items(), key=lambda item: item[1], reverse=True))
-                syl = self.choose_per_deviation(list(sorted_syl_choice.keys()), params['deviation'])
-            name.append(syl)
+                continue
+
         name = ''.join(name)
-        return name.capitalize()
+        return {'name': name.capitalize(), 'params': params}
 
 
 app = Flask(__name__)
@@ -173,9 +158,8 @@ p.parse_counts()
 @app.route("/api/generate_name/<int:intelligence>/<int:strength>/<int:speed>/<int:honesty>/<int:compassion>")
 @cross_origin()
 def run(intelligence, strength, speed, honesty, compassion):
-    name = p.generate_name(intelligence, strength, speed, honesty, compassion)
-    response = flask.jsonify({'name': name})
-    # response.headers.add('Access-Control-Allow-Origin', '127.0.0.1:3000')
+    res = p.generate_name(intelligence, strength, speed, honesty, compassion)
+    response = flask.jsonify(res)
     return response
 
 
